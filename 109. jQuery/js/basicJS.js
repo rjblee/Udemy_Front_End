e3ddd1b5a5df5cb95f4301e0e6723d662a5544c6
Ralph <ralph.lee@live.ca>
2020-03-15 01:25:46 +0000

    Add toggle effect

--- 109. jQuery/js/basicJS.js
+++ 109. jQuery/js/basicJS.js
@@ -1,9 +1,16 @@
-$(document).ready(function() {
-    $(".h1").click(function() {
-        $("h1").hide();
-    });
-    $(".p").click(function() {
-        $("p").hide();
-    })
+// $(document).ready(function() {
+//     $(".h1").click(function() {
+//         $("h1").hide();
+//     });
+//     $(".p").click(function() {
+//         $("p").hide();
+//     })
 
-});
+// });
+
+
+$(function() {
+    $("h1").click(function() {
+        $("img").toggle(1000)
+    })
+})
